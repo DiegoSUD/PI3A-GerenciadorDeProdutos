acadba346c58731e76c697a6bb54b2d7e53d9f52
diegoSUD <>
2018-02-28 19:22:32 -0300

    Telas prontas e classes criadas.

---
+++ PI3A-GerenciadorDeProdutos/src/main/java/br/com/erro404/pi3a/gerenciadordeprodutos/classes/Produto.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.erro404.pi3a.gerenciadordeprodutos.classes;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author gabriel
+ */
+public class Produto {
+    private int id;
+    private String nome;
+    private String descricao;
+    private Double precoCompra;
+    private Double precoVenda;
+    private Integer quantidade;
+    private Date dataCadastro;
+    private Categoria categoria = new Categoria();
+    private ArrayList<Categoria> categorias;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPrecoCompra() {
+        return precoCompra;
+    }
+
+    public void setPrecoCompra(Double precoCompra) {
+        this.precoCompra = precoCompra;
+    }
+
+    public Double getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setPrecoVenda(Double precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+}
